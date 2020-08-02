f121139f54880523e1ef248369e2ec29d1ce5b48
alexandresantosm <amenezes031@gmail.com>
2020-08-02 10:21:49 -0300

    Add bold functionality.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const richTextField = document.querySelector('#richTextField');
+const boldButton = document.querySelector('#bold');
 
+// Enable edit mode for iframe
 function enableEditMode() {
   richTextField.contentDocument.designMode = 'on';
 }
 
+function executeCommand(command) {
+  richTextField.contentDocument.execCommand(command, false, null);
+}
+
+// Start editable mode
 function init() {
   enableEditMode()
 }
 
+// Executes the init function when the HTML loads on the page
 window.onload = init;
