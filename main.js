cf790e0fe5d21c77b9e9bbda7b1995791a262dab
alexandresantosm <amenezes031@gmail.com>
2020-08-02 17:44:26 -0300

    Resizing the app.

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Cria uma janela de navegação.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 700,
     titleBarStyle: "hidden",
     alwaysOnTop: true,
     webPreferences: {
